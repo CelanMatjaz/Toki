a194a723a35238e4e7b4a545ae0a9473eb23d371
CelanMatjaz <matjazcelangithub@gmail.com>
2024-03-31 07:45:15 +0200

    add wrapper ref

--- toki/src/toki/core/core.h
+++ toki/src/toki/core/core.h
@@ -19,4 +19,31 @@ constexpr Ref<T> createRef(Args&&... args) {
     return std::make_shared<T>(std::forward<Args>(args)...);
 }
 
+template <typename T>
+class WrappedRef {
+public:
+    WrappedRef(T* ref) : m_ref(ref){};
+    WrappedRef(const T* ref) : m_ref(ref){};
+    ~WrappedRef() = default;
+    WrappedRef(const WrappedRef<T>& other) : m_ref(other.m_ref) {}
+    WrappedRef(WrappedRef<T>&& other) : m_ref(other.m_ref) {}
+    WrappedRef<T>& operator=(const WrappedRef<T>& other) { return *this = WrappedRef<T>(other); }
+    WrappedRef<T>& operator=(WrappedRef<T>&& other) {
+        m_ref = other.m_ref;
+        return *this;
+    }
+    const T* get() const { return m_ref; }
+
+private:
+    const T* m_ref;
+};
+template <typename T>
+constexpr WrappedRef<T> createWrappedRef(T* ref) {
+    return ref;
+}
+template <typename T>
+constexpr WrappedRef<T> createWrappedRef(const T* ref) {
+    return ref;
+}
+
 }  // namespace Toki
