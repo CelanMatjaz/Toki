699a603edf918964b1c9375cf5918aa0bb3a379f
CelanMatjaz <matjazcelangithub@gmail.com>
2025-01-04 23:53:16 +0100

    add id

---
+++ toki_engine/src/toki/core/id.cpp
@@ -0,0 +1,68 @@
+#include "id.h"
+
+#include <ctime>
+
+namespace Toki {
+
+Id Id::createId() {
+    return Id();
+}
+
+static uint32_t id = 0;
+
+Id::Id(): m_values{ .u32{ ~(id++), (uint32_t) time(0) } } {}
+
+Id::Id(uint64_t n): m_values{ n } {}
+
+Id::Id(const Id& other) {
+    m_values.u64 = other.m_values.u64;
+}
+
+Id::Id(Id&& other) {
+    m_values.u64 = other.m_values.u64;
+}
+
+uint64_t Id::getId() const {
+    return m_values.u64;
+}
+
+Id& Id::operator=(const Id& other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    m_values.u64 = other.m_values.u64;
+    return *this;
+}
+
+Id& Id::operator=(const Id&& other) {
+    if (this == &other) {
+        return *this;
+    }
+
+     m_values.u64 = other.m_values.u64;
+    return *this;
+}
+
+Id& Id::operator=(const uint64_t&& n) {
+    m_values.u64 = n;
+    return *this;
+}
+
+bool Id::operator==(const Id& other) const {
+    return m_values.u64 == other.m_values.u64;
+}
+
+bool Id::operator<(const Id& other) const {
+    return m_values.u64 < other.m_values.u64;
+}
+
+uint64_t Id::operator()(const Id& p) const {
+    return p.m_values.u64;
+}
+
+Id::operator bool() const {
+    return m_values.u64 != 0;
+}
+
+}  // namespace Toki
---
+++ toki_engine/src/toki/core/id.h
@@ -0,0 +1,36 @@
+
+#pragma once
+
+#include <cstdint>
+
+namespace Toki {
+
+class Id {
+public:
+    static Id createId();
+
+    Id();
+    Id(uint64_t n);
+    Id(const Id& other);
+    Id(Id&& other);
+    Id& operator=(const Id& other);
+    Id& operator=(const Id&& other);
+    Id& operator=(const uint64_t&& n);
+    ~Id() = default;
+
+    uint64_t getId() const;
+
+    bool operator==(const Id& other) const;
+    bool operator<(const Id& other) const;
+    uint64_t operator()(const Id& p) const;
+
+    operator bool() const;
+
+private:
+    union {
+        uint64_t u64;
+        uint32_t u32[2];
+    } m_values;
+};
+
+}  // namespace Toki
